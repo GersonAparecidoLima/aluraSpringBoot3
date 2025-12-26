ba6f9c8bb99f506fb3a2b747cca6f1a5a3cc20b8
GersonAparecidoLima <gersonaparecido@gmail.com>
2025-12-26 14:42:51 -0300

    Record chamamos de padrao DTO - Data Transfer Object (Objeto de transferencia de dados).

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,16 +5,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import med.voll.api.medico.DadosCadastroMedico;
+
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 	
 	//Essa é uma das maneiras de recebermos dados nos métodos dos controllers: 
 	//declarando como string e anotando como request body, no caso de requisições do tipo post
+	//@PostMapping
+	//public void cadastrar(@RequestBody String json) {
+	//	System.out.println(json);
+	//}
+	
 	@PostMapping
-	public void cadastrar(@RequestBody String json) {
-		System.out.println(json);
-				
+	public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+		System.out.println(dados);
 	}
 
 }
