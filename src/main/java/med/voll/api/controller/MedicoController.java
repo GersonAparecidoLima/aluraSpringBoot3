9c0299c2a18bf36736b4c20541b736d95aa04822
GersonAparecidoLima <gersonaparecido@gmail.com>
2025-12-26 14:09:04 -0300

    recebermos dados nos métodos dos controllers

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,20 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+	
+	//Essa é uma das maneiras de recebermos dados nos métodos dos controllers: 
+	//declarando como string e anotando como request body, no caso de requisições do tipo post
+	@PostMapping
+	public void cadastrar(@RequestBody String json) {
+		System.out.println(json);
+				
+	}
+
+}
